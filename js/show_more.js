bda9556a1b0c4602d84039597e8ccff0b93f2f78
lippertmark <lippert.mark.d@gmail.com>
2022-06-05 02:41:54 +0300

    added logo and link to contacts

--- js/show_more.js
+++ js/show_more.js
@@ -1,3 +1,13 @@
+
 function showAnswer(target_div) {
-    document.getElementById(target_div).style.display = 'block';
+    if (document.getElementById(target_div).style.display == 'flex'){
+        document.getElementById(target_div).style.display = 'none';
+    }
+    else{
+        document.getElementById(target_div).style.display = 'flex';
+    }
+
+}
+function hideDiv(target_div) {
+    document.getElementById(target_div).style.display = 'none';
 }
