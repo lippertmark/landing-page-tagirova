a5a9c21abba131854ebbd7bd5e0a177d025690a0
lippertmark <lippert.mark.d@gmail.com>
2022-07-20 19:59:57 +0500

    useless ph number

--- send.php
+++ send.php
@@ -8,7 +8,7 @@ $quest = $_POST['quest'];
 $apiToken = "";
 $data = [
    'chat_id' => '-1001513821178',
-   'text' => "Имя: " . $name . "\nНомер телефона: " . $phone . "\nВопрос:" .  $quest];
+   'text' => "Имя: " . $name . "\nНомер телефона: `" . $phone . "`\nВопрос:" .  $quest];
 $response = file_get_contents("https://api.telegram.org/bot$apiToken/sendMessage?" . http_build_query($data) );
 header(“Location: http://yurist-tagirova.ru”);
 exit( );
