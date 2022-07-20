8310632f746290553a299059b2b8463283630597
lippertmark <lippert.mark.d@gmail.com>
2022-07-20 19:50:20 +0500

    question edits

--- send.php
+++ send.php
@@ -5,9 +5,11 @@ $phone = $_POST['phone'];
 $quest = $_POST['quest'];
 
 
-$apiToken = "5388289094:AAFJFkNbOvR7ZfP5x6_8kh48H9fqqjp3jXQ";
+$apiToken = "";
 $data = [
    'chat_id' => '-1001513821178',
    'text' => "Имя: " . $name . "\nНомер телефона: " . $phone . "\nВопрос:" .  $quest];
 $response = file_get_contents("https://api.telegram.org/bot$apiToken/sendMessage?" . http_build_query($data) );
+header(“Location: http://yurist-tagirova.ru”);
+exit( );
 ?>
