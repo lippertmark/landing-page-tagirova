2228dd661c82cd8a927f70711d533d1f0440b39e
lippertmark <lippert.mark.d@gmail.com>
2022-07-20 19:44:17 +0500

    question edits

--- send.php
+++ send.php
@@ -7,7 +7,7 @@ $quest = $_POST['quest'];
 
 $apiToken = "5388289094:AAFJFkNbOvR7ZfP5x6_8kh48H9fqqjp3jXQ";
 $data = [
-   'chat_id' => '338600505',
-   'text' => "Имя: " . $name . "\n номер телефона: " . $phone . "\n" .  $quest];
+   'chat_id' => '-1001513821178',
+   'text' => "Имя: " . $name . "\nНомер телефона: " . $phone . "\nВопрос:" .  $quest];
 $response = file_get_contents("https://api.telegram.org/bot$apiToken/sendMessage?" . http_build_query($data) );
 ?>
